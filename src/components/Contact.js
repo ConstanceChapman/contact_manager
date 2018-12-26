6bfc13637e1b6c890b1f5ff2300d5d44ccd44c35
Constance Chapman <constancealicechapman224@gmail.com>
2018-12-26 15:03:25 +0000

    create onclick function for component display

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = () => {
+    this.setState({showContactInfo: !this.state.showContactInfo});
+  };
+
   render() {
     const { email, name, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
+        <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
+        {showContactInfo ? (<ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        </ul>) : null}
       </div>
     );
   };
@@ -18,6 +28,6 @@ class Contact extends Component {
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired
-}
+};
 
 export default Contact;
